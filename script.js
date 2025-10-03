105b22e082cdbb135d540e84f89f950ed76695b2
steventauriello <stauriello86@gmail.com>
2025-10-03 08:58:12 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("learnMoreBtn");
+  
+  btn.addEventListener("click", () => {
+    alert("Veteran Verify is under construction — stay tuned!");
+  });
+});
